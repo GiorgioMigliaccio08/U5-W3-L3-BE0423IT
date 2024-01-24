6eb0a1a528f0d19fa4d043fa01915185902e4ee4
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2024-01-24 16:25:05 +0100

    Initial commit

---
+++ src/main/java/giorgiomigliaccio/U5W3L3/entities/UserData.java
@@ -0,0 +1,40 @@
+package giorgiomigliaccio.U5W3L3.entities;
+
+import javax.sql.DataSource;
+
+public class UserData {
+    private String nomeCompleto;
+    private int eta;
+    public void getData (DataSource ds) (
+    nomeCompleto = ds. getNomeCompleto ();
+    eta = ds-getEta();
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public int getEta() {
+        return eta;
+    }
+
+    public void setEta(int eta) {
+        this.eta = eta;
+    }
+
+    public UserData(String nomeCompleto, int eta) {
+        this.nomeCompleto = nomeCompleto;
+        this.eta = eta;
+    }
+
+    @Override
+    public String toString() {
+        return "UserData{" +
+                "nomeCompleto='" + nomeCompleto + '\'' +
+                ", eta=" + eta +
+                '}';
+    }
+}
---
+++ src/test/java/giorgiomigliaccio/U5W3L3/U5W3L3ApplicationTests.java
@@ -0,0 +1,13 @@
+package giorgiomigliaccio.U5W3L3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W3L3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
